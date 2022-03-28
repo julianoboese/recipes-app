21718d299184b9fad7e89181852a8e1ef8f73d03
Victor Hugo <victorhsmsantos@gmail.com>
2022-03-28 16:20:08 -0300

    Componente SearchBar criado

---
+++ src/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+function SearchBar() {
+  return (
+    <div>
+      <input
+        placeholder="Search Recipe"
+      />
+    </div>
+  );
+}
+
+SearchBar.propTypes = {
+};
+
+export default SearchBar;
