0adcfab9f76530fc02de698bdf106e2005769f08
Juliano Boese <juliano.boese@gmail.com>
2022-03-30 15:23:40 -0300

    Utiliza botão componentizado de compartilhar

--- src/App.js
+++ src/App.js
@@ -23,22 +23,22 @@ function App() {
   return (
     <RecipesProvider>
       <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/drinks" component={MainDrinkRecipes} />
-        <Route exact path="/foods" component={MainMealRecipes} />
-        <Route exact path="/foods/:recipeId/in-progress" component={MealInProgress} />
-        <Route exact path="/drinks/:recipeId/in-progress" component={DrinkInProgress} />
-        <Route path="/foods/:id" render={(props) => <MealRecipe {...props} />} />
-        <Route path="/drinks/:id" render={(props) => <DrinkRecipe {...props} />} />
-        <Route exact path="/profile" component={Profile} />
-        <Route exact path="/favorite-recipes" component={FavoriteRecipes} />
-        <Route exact path="/done-recipes" component={DoneRecipes} />
-        <Route exact path="/explore" component={Explore} />
-        <Route exact path="/explore/foods" component={ExploreFoods} />
-        <Route exact path="/explore/drinks" component={ExploreFoods} />
-        <Route path="/explore/foods/ingredients" component={ExploreIngredients} />
-        <Route path="/explore/drinks/ingredients" component={ExploreIngredients} />
-        <Route path="/explore/foods/nationalities" component={ExploreNationality} />
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/drinks" component={ MainDrinkRecipes } />
+        <Route exact path="/foods" component={ MainMealRecipes } />
+        <Route exact path="/foods/:recipeId/in-progress" component={ MealInProgress } />
+        <Route exact path="/drinks/:recipeId/in-progress" component={ DrinkInProgress } />
+        <Route path="/foods/:id" render={ (props) => <MealRecipe { ...props } /> } />
+        <Route path="/drinks/:id" render={ (props) => <DrinkRecipe { ...props } /> } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
+        <Route exact path="/done-recipes" component={ DoneRecipes } />
+        <Route exact path="/explore" component={ Explore } />
+        <Route exact path="/explore/foods" component={ ExploreFoods } />
+        <Route exact path="/explore/drinks" component={ ExploreFoods } />
+        <Route path="/explore/foods/ingredients" component={ ExploreIngredients } />
+        <Route path="/explore/drinks/ingredients" component={ ExploreIngredients } />
+        <Route path="/explore/foods/nationalities" component={ ExploreNationality } />
         <Route path="*" component={ NotFound } />
       </Switch>
     </RecipesProvider>
