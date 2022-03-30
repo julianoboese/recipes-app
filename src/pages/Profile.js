9c5c4e481f3a93f8ec10939dce18bf853f5bd249
Juliano Boese <juliano.boese@gmail.com>
2022-03-30 11:55:01 -0300

    Req 11 - Redireciona para tela de Perfil ao clicar no Header

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import Header from '../components/Header';
 
-function Profile({ history }) {
+function Profile({ history, location }) {
   const getEmail = () => {
-    const jsonUser = JSON.parse(localStorage.getItem('user'));
-    console.log(jsonUser);
+    const jsonUser = JSON.parse(localStorage.getItem('user')) || { email: '' };
     return jsonUser.email;
   };
 
@@ -22,38 +22,44 @@ function Profile({ history }) {
   };
 
   return (
-    <div>
-      <p data-testid="profile-email">
-        { getEmail() }
-      </p>
-      <button
-        type="button"
-        data-testid="profile-done-btn"
-        onClick={ redirectDoneRecipes }
-      >
-        Done Recipes
-      </button>
-      <button
-        type="button"
-        data-testid="profile-favorite-btn"
-        onClick={ redirectFavoriteRecipes }
-      >
-        Favorite Recipes
-      </button>
-      <button
-        type="button"
-        data-testid="profile-logout-btn"
-        onClick={ redirectLoginPage }
-      >
-        Logout
-      </button>
-    </div>
+    <>
+      <Header history={ history } location={ location.pathname } />
+      <div>
+        <p data-testid="profile-email">
+          { getEmail() }
+        </p>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ redirectDoneRecipes }
+        >
+          Done Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ redirectFavoriteRecipes }
+        >
+          Favorite Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ redirectLoginPage }
+        >
+          Logout
+        </button>
+      </div>
+    </>
   );
 }
 
 Profile.propTypes = {
   history: PropTypes.shape({
     push: PropTypes.func,
+  }).isRequired,
+  location: PropTypes.shape({
+    pathname: PropTypes.string,
   }).isRequired,
 };
 
