c311d0af17e11c970c5ddfce859a11476effa285
Juliano Boese <juliano.boese@gmail.com>
2022-04-04 17:10:28 -0300

    FavoriteBtn 100% coverage

--- src/tests/FavoriteBtn.test.js
+++ src/tests/FavoriteBtn.test.js
@@ -3,17 +3,65 @@ import userEvent from '@testing-library/user-event';
 import { renderUrl } from './helpers/renderUrl';
 import '@testing-library/jest-dom';
 
-describe('Favorite Button', () => {
-  beforeEach(() => {
-    localStorage.setItem('doneRecipes', JSON.stringify(doneRecipes));
-    renderUrl('/done-recipes');
-  });
-  afterEach(() => localStorage.clear());
+describe('Favorite Button - add favorites', () => {
+  it('should add recipe to favorites on button click', async () => {
+    renderUrl('/foods/52771');
 
-  it('should have three filters: All, Food and Drink', async () => {
-    const allBtn = await screen.findByRole('button', { name: /All/i });
-    const foodsBtn = await screen.findByRole('button', { name: /Food/i });
-    const drinksBtn = await screen.findByRole('button', { name: /Drinks/i });
-    expect(allBtn && foodsBtn && drinksBtn).toBeInTheDocument();
+    const notFavoriteBtn = await screen.findByAltText('ícone de não favorito');
+    const favoriteBtn = screen.queryByAltText('ícone de favorito');
+    expect(localStorage.getItem('favoriteRecipes')).toBeNull();
+    expect(notFavoriteBtn).toBeInTheDocument();
+    expect(favoriteBtn).not.toBeInTheDocument();
+
+    userEvent.click(notFavoriteBtn);
+    expect(JSON.parse(localStorage.getItem('favoriteRecipes')))
+      .toEqual([{ id: '52771',
+        type: 'food',
+        nationality: 'Italian',
+        category: 'Vegetarian',
+        alcoholicOrNot: '',
+        name: 'Spicy Arrabiata Penne',
+        image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg' }]);
+  });
+
+  it('should add another recipe to favorites on button click', async () => {
+    renderUrl('/drinks/178319');
+    const notFavoriteBtn = await screen.findByAltText('ícone de não favorito');
+
+    userEvent.click(notFavoriteBtn);
+    expect(JSON.parse(localStorage.getItem('favoriteRecipes')))
+      .toEqual([{ id: '52771',
+        type: 'food',
+        nationality: 'Italian',
+        category: 'Vegetarian',
+        alcoholicOrNot: '',
+        name: 'Spicy Arrabiata Penne',
+        image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg' },
+      { id: '178319',
+        type: 'drink',
+        nationality: '',
+        category: 'Cocktail',
+        alcoholicOrNot: 'Alcoholic',
+        name: 'Aquamarine',
+        image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg' }]);
+  });
+});
+
+describe('Favorite Button - remove favorite', () => {
+  it('should remove recipe from favorites on button click', async () => {
+    renderUrl('/foods/52771');
+
+    const favoriteBtn = await screen.findByAltText('ícone de favorito');
+    expect(favoriteBtn).toBeInTheDocument();
+
+    userEvent.click(favoriteBtn);
+    expect(JSON.parse(localStorage.getItem('favoriteRecipes')))
+      .toEqual([{ id: '178319',
+        type: 'drink',
+        nationality: '',
+        category: 'Cocktail',
+        alcoholicOrNot: 'Alcoholic',
+        name: 'Aquamarine',
+        image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg' }]);
   });
 });
