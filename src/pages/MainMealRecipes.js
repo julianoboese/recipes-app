13c029e0248fedef674f3ee6c5800773c0b7c9d6
Brenno Miliani <brennomiliani@hotmail.com>
2022-04-07 15:53:42 -0300

    fixing search bugs

--- src/pages/MainMealRecipes.js
+++ src/pages/MainMealRecipes.js
@@ -1,50 +1,23 @@
 import PropTypes from 'prop-types';
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import MealCard from '../components/MealCard';
 import RecipesContext from '../context/RecipesContext';
 import {
-  fetchMealCategories,
   fetchMeals,
   fetchMealsByCategory,
 } from '../services/fetchMeals';
 import MainStyled from '../default_styles/MainStyle';
+import useMainRecipes from '../hooks/useMainRecipes';
 
 function MainMealRecipes({ history, location }) {
-  const { currentRecipes, setCurrentRecipes, ingredients, setIngredients,
-    searchResults, setSearchResults } = useContext(RecipesContext);
+  const { currentRecipes, setCurrentRecipes } = useContext(RecipesContext);
 
-  const [mealCategories, setMealCategories] = useState([]);
   // const [caterogryEmoji, setCaterogryEmoji] = useState([]);
   const [currentCategory, setCurrentCategory] = useState('');
 
-  useEffect(() => {
-    const getCategoriesAndMeals = async () => {
-      const categories = await fetchMealCategories();
-      setMealCategories(categories);
-
-      if (searchResults.length > 0) {
-        setCurrentRecipes(searchResults);
-      } else if (ingredients.length > 0) {
-        const MAX_RECIPES = 12;
-        setCurrentRecipes(ingredients.splice(0, MAX_RECIPES));
-      } else {
-        const meals = await fetchMeals();
-        setCurrentRecipes(meals);
-      }
-    };
-    getCategoriesAndMeals();
-  }, [setCurrentRecipes, ingredients, searchResults]);
-
-  useEffect(() => () => {
-    if (ingredients.length > 0) {
-      setIngredients([]);
-    }
-    if (searchResults.length > 0) {
-      setSearchResults([]);
-    }
-  }, [ingredients, setIngredients, searchResults, setSearchResults]);
+  const [recipeCategories, isLoading] = useMainRecipes(location.pathname);
 
   const handleCategory = async ({ target }) => {
     if (target.innerHTML === currentCategory || target.innerHTML === 'All') {
@@ -61,34 +34,36 @@ function MainMealRecipes({ history, location }) {
   return (
     <>
       <Header location={ location.pathname } history={ history } />
-      <MainStyled>
-        <section className="category-section">
-          <button
-            type="button"
-            data-testid="All-category-filter"
-            onClick={ handleCategory }
-            className="category-item"
-          >
-            All
-          </button>
-          {mealCategories.map(({ strCategory }) => (
+      {!isLoading && (
+        <MainStyled>
+          <section className="category-section">
             <button
-              key={ strCategory }
               type="button"
-              data-testid={ `${strCategory}-category-filter` }
+              data-testid="All-category-filter"
               onClick={ handleCategory }
               className="category-item"
             >
-              { strCategory}
+              All
             </button>
-          ))}
-        </section>
-        <section className="card-section">
-          {currentRecipes.map((meal, index) => (
-            <MealCard key={ index } meal={ meal } index={ index } />
-          ))}
-        </section>
-      </MainStyled>
+            {recipeCategories.map(({ strCategory }) => (
+              <button
+                key={ strCategory }
+                type="button"
+                data-testid={ `${strCategory}-category-filter` }
+                onClick={ handleCategory }
+                className="category-item"
+              >
+                { strCategory}
+              </button>
+            ))}
+          </section>
+          <section className="card-section">
+            {currentRecipes.map((meal, index) => (
+              <MealCard key={ index } meal={ meal } index={ index } />
+            ))}
+          </section>
+        </MainStyled>
+      )}
       <Footer history={ history } />
     </>
   );
